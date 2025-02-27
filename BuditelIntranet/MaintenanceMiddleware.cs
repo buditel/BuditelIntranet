a6ea0dd5f8219eea5527ab0c6799f9fca5be1604
lechev95 <lecho.lechev@gmail.com>
2025-02-27 11:34:33 +0200

    Add project files.

---
+++ BuditelIntranet/MaintenanceMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace BuditelIntranet
+{
+    public class MaintenanceMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IConfiguration _configuration;
+
+        public MaintenanceMiddleware(RequestDelegate next, IConfiguration configuration)
+        {
+            _next = next;
+            _configuration = configuration;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            bool isMaintenanceMode = _configuration.GetValue<bool>("MaintenanceMode:Enabled");
+
+            if (isMaintenanceMode)
+            {
+                context.Response.ContentType = "text/html";
+                await context.Response.SendFileAsync(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "maintenance.html"));
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+
+}
